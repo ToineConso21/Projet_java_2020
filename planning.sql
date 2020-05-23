e7de37d47b075946b27780fd0f765d85825eb275
Bangio19 <bparamburu@gmail.com>
2020-05-23 12:38:14 +0200

    Create planning.sql

---
+++ planning.sql
@@ -0,0 +1,297 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  sam. 23 mai 2020 à 10:37
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `planning`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cours`
+--
+
+DROP TABLE IF EXISTS `cours`;
+CREATE TABLE IF NOT EXISTS `cours` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NOM` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `enseignant`
+--
+
+DROP TABLE IF EXISTS `enseignant`;
+CREATE TABLE IF NOT EXISTS `enseignant` (
+  `ID_UTILISATEUR` int(11) NOT NULL,
+  `ID_COURS` int(11) NOT NULL,
+  UNIQUE KEY `ID_UTILISATEUR` (`ID_UTILISATEUR`),
+  KEY `ID_COURS` (`ID_COURS`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `etudiant`
+--
+
+DROP TABLE IF EXISTS `etudiant`;
+CREATE TABLE IF NOT EXISTS `etudiant` (
+  `ID_UTILISATEUR` int(11) NOT NULL,
+  `NUMERO` int(11) NOT NULL,
+  `ID_GROUPE` int(11) NOT NULL,
+  PRIMARY KEY (`ID_UTILISATEUR`),
+  KEY `ID_GROUPE` (`ID_GROUPE`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `groupe`
+--
+
+DROP TABLE IF EXISTS `groupe`;
+CREATE TABLE IF NOT EXISTS `groupe` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NOM` varchar(255) NOT NULL,
+  `ID_PROMOTION` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `ID_PROMOTION` (`ID_PROMOTION`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `promotion`
+--
+
+DROP TABLE IF EXISTS `promotion`;
+CREATE TABLE IF NOT EXISTS `promotion` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NOM` int(255) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `promotion`
+--
+
+INSERT INTO `promotion` (`ID`, `NOM`) VALUES
+(1, 2022),
+(2, 2023),
+(3, 2024);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `salle`
+--
+
+DROP TABLE IF EXISTS `salle`;
+CREATE TABLE IF NOT EXISTS `salle` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NOM` varchar(11) NOT NULL,
+  `CAPACITE` int(11) NOT NULL,
+  `ID_SITE` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `ID_SITE` (`ID_SITE`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `seance`
+--
+
+DROP TABLE IF EXISTS `seance`;
+CREATE TABLE IF NOT EXISTS `seance` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `SEMAINE` int(11) NOT NULL,
+  `DATE` date NOT NULL,
+  `HEURE_DEBUT` time NOT NULL,
+  `HEURE_FIN` time NOT NULL,
+  `ETAT` tinyint(1) NOT NULL,
+  `ID_COURS` int(11) NOT NULL,
+  `ID_TYPE` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `ID_COURS` (`ID_COURS`),
+  KEY `ID_TYPE` (`ID_TYPE`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `seance_enseignants`
+--
+
+DROP TABLE IF EXISTS `seance_enseignants`;
+CREATE TABLE IF NOT EXISTS `seance_enseignants` (
+  `ID_SEANCE` int(11) NOT NULL,
+  `ID_ENSEIGNANT` int(11) NOT NULL,
+  KEY `ID_SEANCE` (`ID_SEANCE`),
+  KEY `ID_ENSEIGNANT` (`ID_ENSEIGNANT`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `seance_groupes`
+--
+
+DROP TABLE IF EXISTS `seance_groupes`;
+CREATE TABLE IF NOT EXISTS `seance_groupes` (
+  `ID_SEANCE` int(11) NOT NULL,
+  `ID_GROUPE` int(11) NOT NULL,
+  KEY `ID_GROUPE` (`ID_GROUPE`),
+  KEY `ID_SEANCE` (`ID_SEANCE`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `seance_salles`
+--
+
+DROP TABLE IF EXISTS `seance_salles`;
+CREATE TABLE IF NOT EXISTS `seance_salles` (
+  `ID_SEANCE` int(11) NOT NULL,
+  `ID_SALLE` int(11) NOT NULL,
+  KEY `ID_SEANCE` (`ID_SEANCE`),
+  KEY `ID_SALLE` (`ID_SALLE`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `site`
+--
+
+DROP TABLE IF EXISTS `site`;
+CREATE TABLE IF NOT EXISTS `site` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NOM` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `type_cours`
+--
+
+DROP TABLE IF EXISTS `type_cours`;
+CREATE TABLE IF NOT EXISTS `type_cours` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NOM` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `EMAIL` varchar(255) NOT NULL,
+  `MDP` varchar(255) NOT NULL,
+  `NOM` varchar(255) NOT NULL,
+  `PRENOM` varchar(255) NOT NULL,
+  `DROIT` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`ID`, `EMAIL`, `MDP`, `NOM`, `PRENOM`, `DROIT`) VALUES
+(1, 'user1@edu.ece.fr', 'user1', 'Premier', 'User', 1),
+(2, 'user2@edu.ece.fr', 'user2', 'Deuxieme', 'User', 2),
+(3, 'user3@edu.ece.fr', 'user3', 'Troisieme', 'User', 3),
+(4, 'user4@edu.ece.fr', 'user4', 'Quatrieme', 'User', 4);
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `enseignant`
+--
+ALTER TABLE `enseignant`
+  ADD CONSTRAINT `enseignant_ibfk_1` FOREIGN KEY (`ID_UTILISATEUR`) REFERENCES `utilisateur` (`ID`),
+  ADD CONSTRAINT `enseignant_ibfk_2` FOREIGN KEY (`ID_COURS`) REFERENCES `cours` (`ID`);
+
+--
+-- Contraintes pour la table `etudiant`
+--
+ALTER TABLE `etudiant`
+  ADD CONSTRAINT `etudiant_ibfk_2` FOREIGN KEY (`ID_UTILISATEUR`) REFERENCES `utilisateur` (`ID`),
+  ADD CONSTRAINT `etudiant_ibfk_3` FOREIGN KEY (`ID_GROUPE`) REFERENCES `groupe` (`ID`);
+
+--
+-- Contraintes pour la table `groupe`
+--
+ALTER TABLE `groupe`
+  ADD CONSTRAINT `groupe_ibfk_1` FOREIGN KEY (`ID_PROMOTION`) REFERENCES `promotion` (`ID`);
+
+--
+-- Contraintes pour la table `salle`
+--
+ALTER TABLE `salle`
+  ADD CONSTRAINT `salle_ibfk_1` FOREIGN KEY (`ID_SITE`) REFERENCES `site` (`ID`);
+
+--
+-- Contraintes pour la table `seance`
+--
+ALTER TABLE `seance`
+  ADD CONSTRAINT `seance_ibfk_1` FOREIGN KEY (`ID_COURS`) REFERENCES `cours` (`ID`),
+  ADD CONSTRAINT `seance_ibfk_2` FOREIGN KEY (`ID_TYPE`) REFERENCES `type_cours` (`ID`);
+
+--
+-- Contraintes pour la table `seance_enseignants`
+--
+ALTER TABLE `seance_enseignants`
+  ADD CONSTRAINT `seance_enseignants_ibfk_1` FOREIGN KEY (`ID_ENSEIGNANT`) REFERENCES `enseignant` (`ID_UTILISATEUR`),
+  ADD CONSTRAINT `seance_enseignants_ibfk_2` FOREIGN KEY (`ID_SEANCE`) REFERENCES `seance` (`ID`);
+
+--
+-- Contraintes pour la table `seance_groupes`
+--
+ALTER TABLE `seance_groupes`
+  ADD CONSTRAINT `seance_groupes_ibfk_1` FOREIGN KEY (`ID_GROUPE`) REFERENCES `groupe` (`ID`),
+  ADD CONSTRAINT `seance_groupes_ibfk_2` FOREIGN KEY (`ID_SEANCE`) REFERENCES `seance` (`ID`);
+
+--
+-- Contraintes pour la table `seance_salles`
+--
+ALTER TABLE `seance_salles`
+  ADD CONSTRAINT `seance_salles_ibfk_1` FOREIGN KEY (`ID_SEANCE`) REFERENCES `seance` (`ID`),
+  ADD CONSTRAINT `seance_salles_ibfk_2` FOREIGN KEY (`ID_SALLE`) REFERENCES `salle` (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
